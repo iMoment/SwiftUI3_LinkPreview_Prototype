0dbd0988747510336eed037a1abab61d22b081ae
Stanley Pan <imoment.developer@gmail.com>
2022-02-17 16:11:03 -0500

    Initial Commit

---
+++ LinkPreview/LinkPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkPreviewApp.swift
+//  LinkPreview
+//
+//  Created by Stanley Pan on 2022/02/17.
+//
+
+import SwiftUI
+
+@main
+struct LinkPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
